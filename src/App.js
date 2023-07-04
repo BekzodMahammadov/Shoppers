4141909f9e37bcd047a74dcb83d467b9e76cce79
Mahammadov Bekzod <bekzodmahammadov627@gmail.com>
2023-07-04 15:13:40 +0500

    my react app started

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="text-3xl font-bold underline w-full h-4 bg-red-600 text-red-600">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
